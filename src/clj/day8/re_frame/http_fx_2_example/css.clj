a742ed2f8eec4962959df9d2a1f253b7e2473760
Isaac Johnston <isaac@superstruct.nz>
2019-08-05 16:32:47 +0300

    Add styles for react-file-drop

--- src/clj/day8/re_frame/http_fx_2_example/css.clj
+++ src/clj/day8/re_frame/http_fx_2_example/css.clj
@@ -1,6 +1,9 @@
 (ns day8.re-frame.http-fx-2-example.css
   (:require
-    [garden.def :refer [defstyles defkeyframes]]))
+    [garden.color :refer [rgba]]
+    [garden.def :refer [defstyles defkeyframes]]
+    [garden.selectors :refer [>]])
+  (:refer-clojure :exclude [>]))
 
 (defstyles screen
   [:body
@@ -19,4 +22,35 @@
    [:.rc-md-icon-button
     :.rc-title
     {:color "#DDD"
-     :font-size "31px"}]])
+     :font-size "31px"}]]
+
+  [:.file-drop
+   {:position "relative"
+    :height "100px"
+    :width "100%"}]
+
+  [(> :.file-drop :.file-drop-target)
+   {:position "absolute"
+    :top 0
+    :left 0
+    :height "100%"
+    :width "100%"
+    :border-radius "2px"
+    :display "flex"
+    :flex-direction "column"
+    :align-items "center"
+    :justify-content "center"
+    :align-content "center"
+    :text-align "center"}]
+
+  [(> :.file-drop :.file-drop-target.file-drop-dragging-over-frame)
+   {:border "none"
+    :background-color (rgba 0 0 0 0.65)
+    :box-shadow "none"
+    :z-index 50
+    :opacity 1.0
+    :color "white"}]
+
+  [(> :.file-drop :.file-drop-target.file-drop-dragging-over-target)
+   {:color "#ff6e40"
+    :box-shadow "0 0 13px 3px #ff6e40"}])
