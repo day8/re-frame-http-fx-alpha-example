768f2a315e9b37e4087cef172c57b3d06d92d756
Isaac Johnston <isaac@superstruct.nz>
2019-08-28 09:33:05 +0800

    Upgrade shadow-cljs to 2.8.52

--- src/cljs/deps.cljs
+++ src/cljs/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-deps     {"react-file-drop" "0.2.8"}
- :npm-dev-deps {"shadow-cljs"           "2.8.51"
+ :npm-dev-deps {"shadow-cljs"           "2.8.52"
                 "karma"                 "4.2.0"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"}}
